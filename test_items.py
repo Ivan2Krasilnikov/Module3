097f283df39b91b286f436ae28c485bb532b1b74
Ivan <56519101+Razer725@users.noreply.github.com>
2021-05-09 18:44:46 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 
-def test_shopcart_available(browser):
+def test_shopcart_button_is_displayed(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     browser.implicitly_wait(2)
