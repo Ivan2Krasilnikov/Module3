0512530bcffb4c764bd1431930e2178cf73331f8
Ivan <56519101+Razer725@users.noreply.github.com>
2021-05-09 18:29:03 +0300

    First commit

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+
+def test_shopcart_available(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    browser.implicitly_wait(2)
+    time.sleep(10)
+    assert len(browser.find_elements_by_class_name("btn-primary")) > 0, "Кнопка 'Добавить в корзину' не найдена"
